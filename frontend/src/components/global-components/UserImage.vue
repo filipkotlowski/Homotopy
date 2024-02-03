064501432064bcf1cf56c8510383f90f0739d1c3
filipkotlowski <cotlowzky@gmail.com>
2024-02-04 00:26:58 +0100

    Feature: create user image component

---
+++ frontend/src/components/global-components/UserImage.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+        <div class="rounded-full bg-black w-[36px] h-[36px]"></div>
+</template>
--- frontend/src/components/navigation/horizontal/HorizontalNavbar.vue
+++ frontend/src/components/navigation/horizontal/HorizontalNavbar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+import UserImage from "@/components/global-components/UserImage.vue";
 </script>
 <template>
-    
     <div class="w-[84%] mx-auto mt-5 bg-white h-16 rounded-md shadow-lg shadow-gray-200 border-gray-200 border-2 flex justify-end items-center">
-            <div class="flex justify-around">
-                <vue-feather size="26" type="bell" class="py-2 my-auto" />
-                <div class="py-2 px-4">
-                    <div class="rounded-full bg-black w-[36px] h-[36px]"></div>
-                </div>
+        <div class="flex justify-around">
+            <vue-feather size="26" type="bell" class="py-2 my-auto" />
+            <div class="py-2 px-4">
+                <user-image />
             </div>
+        </div>
     </div>
 </template>
