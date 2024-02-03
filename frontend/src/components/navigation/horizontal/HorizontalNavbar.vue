3327aa2a573dae6f68df53d40dd7f7359327c2d6
filipkotlowski <cotlowzky@gmail.com>
2024-02-04 00:21:22 +0100

    Feature: basic styling of horizontal navbar

--- frontend/src/components/navigation/horizontal/HorizontalNavbar.vue
+++ frontend/src/components/navigation/horizontal/HorizontalNavbar.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+</script>
 <template>
     
-    <div class="w-[84%] mx-auto mt-5 bg-white h-16 rounded-md shadow-lg shadow-gray-200 border-gray-200 border-2">
-        test
+    <div class="w-[84%] mx-auto mt-5 bg-white h-16 rounded-md shadow-lg shadow-gray-200 border-gray-200 border-2 flex justify-end items-center">
+            <div class="flex justify-around">
+                <vue-feather size="26" type="bell" class="py-2 my-auto" />
+                <div class="py-2 px-4">
+                    <div class="rounded-full bg-black w-[36px] h-[36px]"></div>
+                </div>
+            </div>
     </div>
 </template>
