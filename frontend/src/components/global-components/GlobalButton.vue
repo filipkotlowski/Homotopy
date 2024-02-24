8df77034657bf7a486a5d55b94b0c940038b5f0e
filipkotlowski <cotlowzky@gmail.com>
2024-02-24 10:13:47 +0100

    Refactor: create login page using vuetify

---
+++ frontend/src/components/global-components/GlobalButton.vue
@@ -0,0 +1,9 @@
+<script setup type="ts">
+const props = defineProps({
+    text: String,
+});
+</script>
+<template>
+    
+    <v-btn type="submit" block :text="props.text"/>
+</template>
---
+++ frontend/src/components/global-components/GlobalInput.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const props = defineProps({
+    label: String,
+    type: String,
+});
+</script>
+
+<template>
+    <v-text-field :label="props.label" :type="props.type" variant="outlined" />
+</template>
