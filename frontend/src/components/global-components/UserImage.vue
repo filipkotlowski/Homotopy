45cea1af0fa338ccf78f17929f590644cd9aeb11
filipkotlowski <cotlowzky@gmail.com>
2024-03-03 22:37:44 +0100

    Feature: add to user image dropdown with logout option

--- frontend/src/components/global-components/UserImage.vue
+++ frontend/src/components/global-components/UserImage.vue
@@ -1,4 +1,37 @@
 <script setup lang="ts"></script>
+
 <template>
-        <div class="rounded-full bg-black w-[36px] h-[36px]"></div>
+    <div class="hover:cursor-pointer">
+        <v-menu :close-on-content-click="false" location="bottom">
+            <template v-slot:activator="{ props }">
+                <div
+                    class="rounded-full bg-black w-[36px] h-[36px]"
+                    v-bind="props"
+                ></div>
+            </template>
+            <v-card min-width="300">
+                <v-list>
+                    <v-list-item
+                        prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+                        subtitle="Founder of Vuetify"
+                        title="John Leider"
+                    >
+                    </v-list-item>
+                </v-list>
+                <v-list>
+                    <v-list-item>
+                        <div
+                            class="flex items-center w-full hover:cursor-pointer hover:bg-gray-100 p-2 rounded-md"
+                        >
+                            <vue-feather
+                                type="log-out"
+                                class="text-gray-700 flex items-center"
+                            />
+                            <span class="text-gray-700 mx-5">Wyloguj się</span>
+                        </div>
+                    </v-list-item>
+                </v-list>
+            </v-card>
+        </v-menu>
+    </div>
 </template>
