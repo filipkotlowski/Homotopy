b61e77b3941f815a37d36349b77d0114732afe89
filipkotlowski <cotlowzky@gmail.com>
2024-04-15 22:08:13 +0200

    Feature: add translation to tab name

--- frontend/src/views/admin/global-settings/GlobalSettings.vue
+++ frontend/src/views/admin/global-settings/GlobalSettings.vue
@@ -3,15 +3,18 @@ import GlobalCard from "@/components/global-components/GlobalCard.vue"
 import GlobalSettingsGeneralTab from "@/views/admin/global-settings/GlobalSettingsGeneralTab.vue"
 import { ref } from "vue";
 const tab = ref('general')
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
 
 <template>
   <div class="flex justify-center">
     <global-card route="globalSettings">
         <v-tabs v-model="tab" align-tabs="center" color="primary">
-          <v-tab value="general">General</v-tab>
-          <v-tab value="internationalization">Internationalization</v-tab>
-          <v-tab value="template">Template</v-tab>
+          <v-tab value="general">{{ t('tab.globalSettings.general') }}</v-tab>
+          <v-tab value="internationalization">{{ t('tab.globalSettings.internationalization') }}</v-tab>
+          <v-tab value="template">{{ t('tab.globalSettings.template') }}</v-tab>
         </v-tabs>
         <v-window v-model="tab">
           <v-window-item value="general">
