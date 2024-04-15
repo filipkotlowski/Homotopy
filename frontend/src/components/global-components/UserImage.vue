54ec8365110b1df712cf9d07be2c37b23406797c
filipkotlowski <cotlowzky@gmail.com>
2024-04-15 22:35:55 +0200

    Feature: replace all old placeholder for placeholders from placehold.co

--- frontend/src/components/global-components/UserImage.vue
+++ frontend/src/components/global-components/UserImage.vue
@@ -12,24 +12,22 @@ const { t } = useI18n();
     <div class="hover:cursor-pointer">
         <v-menu :close-on-content-click="false" location="bottom">
             <template v-slot:activator="{ props }">
-                <div class="rounded-full bg-black w-[36px] h-[36px]" v-bind="props"></div>
-            </template>
-            <v-card min-width="300">
-                <v-list>
-                    <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-                        subtitle="Founder of Vuetify" title="John Leider">
-                    </v-list-item>
-                </v-list>
-                <v-list>
-                    <v-list-item>
-                        <div class="flex items-center w-full hover:cursor-pointer hover:bg-gray-100 p-2 rounded-md"
-                            @click="logout">
-                            <vue-feather type="log-out" class="text-gray-700 flex items-center" />
-                            <span class="text-gray-700 mx-5">{{ t('button.logout') }}</span>
-                        </div>
-                    </v-list-item>
-                </v-list>
-            </v-card>
-        </v-menu>
-    </div>
+                <v-img height="36" width="36" cover src="https://placehold.co/100x100" class="rounded-full" v-bind="props" />
+</template>
+<v-card min-width="300">
+    <v-list>
+        <v-list-item prepend-avatar="https://placehold.co/100x100" subtitle="Founder of Vuetify" title="John Leider">
+        </v-list-item>
+    </v-list>
+    <v-list>
+        <v-list-item>
+            <div class="flex items-center w-full hover:cursor-pointer hover:bg-gray-100 p-2 rounded-md" @click="logout">
+                <vue-feather type="log-out" class="text-gray-700 flex items-center" />
+                <span class="text-gray-700 mx-5">{{ t('button.logout') }}</span>
+            </div>
+        </v-list-item>
+    </v-list>
+</v-card>
+</v-menu>
+</div>
 </template>
