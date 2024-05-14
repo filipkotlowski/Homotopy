4dbcafda7963e51f3e2bf3d60af1b6d5deebfb4e
filipkotlowski <cotlowzky@gmail.com>
2024-05-14 21:51:12 +0200

    Feature: add placeholder for photo

--- frontend/src/templates/bari/components/first-section/FirstSection.vue
+++ frontend/src/templates/bari/components/first-section/FirstSection.vue
@@ -8,14 +8,17 @@ import BasicButton from '@/templates/bari/components/BasicButton.vue'
     <section class="h-screen bg-bari-primary px-7 py-10">
         <div class="h-screen flex justify-between items-center">
             <navbar />
-            <div class="mt-10 h-[80%] w-full flex flex-col justify-center">
+            <div class="mt-10 h-[80%] w-full flex align-center gap-[20px]">
                 <div class="w-2/3 flex flex-col gap-[52px]">
-                    <!-- CMS: Hero section title -->
-                    <header-text class="text-bari-accent">Lorem Ipsum is simply dummy text</header-text>
-                    <!-- CMS: Hero section subtitle -->
+                    <!-- CMS: First section title -->
+                    <header-text class="text-bari-accent drop-shadow-primary">Lorem Ipsum is simply dummy text</header-text>
+                    <!-- CMS: First section subtitle -->
                     <span class="text-bari-accent">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
-                    <!-- CMS: Hero section button -->
+                    <!-- CMS: First section button -->
                     <basic-button class="text-bari-primary bg-bari-light">lorem ipsum</basic-button>
+                </div>
+                <div class="w-1/3 flex ">
+                <img class="ml-9 object-cover object-left h-[378px] w-[147px] border-[9px] border-solid border-bari-light drop-shadow-primary" src="https://placehold.co/378x270" />
                 </div>
             </div>
         </div>
