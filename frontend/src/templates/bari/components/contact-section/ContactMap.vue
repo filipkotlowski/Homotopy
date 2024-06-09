e080a786a05c7504a07fa59eef5376e758e5a7dd
filipkotlowski <cotlowzky@gmail.com>
2024-06-09 13:46:47 +0200

    Feature: add google map to contact section

---
+++ frontend/src/templates/bari/components/contact-section/ContactMap.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    data: Object,
+});
+
+</script>
+<template>
+    <iframe
+        :src="data.link"
+        :width="data.width" :height="data.height" style="border:0;" allowfullscreen="false" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+</template>
--- frontend/src/templates/bari/components/contact-section/ContactSection.vue
+++ frontend/src/templates/bari/components/contact-section/ContactSection.vue
@@ -3,6 +3,7 @@ import HeaderText from '@/templates/bari/components/HeaderText.vue';
 import SubHeaderText from '@/templates/bari/components/SubHeaderText.vue';
 import Carousel from '@/templates/bari/components/Carousel.vue';
 import ContactForm from '@/templates/bari/components/contact-section/ContactForm.vue';
+import ContactMap from '@/templates/bari/components/contact-section/ContactMap.vue';
 const data = {
     title: 'Section title',
     subtitle: 'Section subtitle Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
@@ -47,14 +48,20 @@ const data = {
                 label: 'Your message',
             },
         ]
+    },
+    map:{
+        width:'350',
+        height:'350',
+        link:'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.0284631393724!2d17.91605607693218!3d50.6822966706428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47105253b025b681%3A0x29318959b1f239b3!2s46-020%20Opole!5e0!3m2!1sen!2spl!4v1717933338024!5m2!1sen!2spl'
     }
 }
 </script>
 <template>
-    <section :class="data.styles.sectionBackground" class="h-[150vh] px-7 py-10 flex flex-col items-center ">
+    <section :class="data.styles.sectionBackground" class="h-[130vh] px-7 py-10 flex flex-col items-center ">
         <header-text :class="data.styles.headerColor" class="text-center">{{ data.title }}</header-text>
         <v-divider thickness="2" :color="data.styles.dividerColor" class="border-opacity-100 w-3/4"></v-divider>
         <sub-header-text :class="data.styles.subHeaderColor" class="text-center">{{ data.subtitle }}</sub-header-text>
         <contact-form :data="data.form" />
+        <contact-map :data="data.map"/>
     </section>
 </template>
