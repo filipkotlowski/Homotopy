dd7a9c3d02c272a7bf47232f2597c70b71eed839
filipkotlowski <cotlowzky@gmail.com>
2024-06-09 14:26:35 +0200

    Feature: createa social section in contact

---
+++ frontend/src/templates/bari/components/contact-section/ContactSocials.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+import SocialButton from '@/templates/bari/components/contact-section/SocialButton.vue';
+
+const props = defineProps({
+    data: Object
+});
+
+</script>
+<template>
+    <div class="flex gap-14 mt-14">
+        <social-button 
+            v-for="item in data.items"
+            :social="item"
+            :styles="data.styles"
+            />
+    </div>
+</template>
---
+++ frontend/src/templates/bari/components/contact-section/SocialButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    social: Object,
+    styles: Object
+});
+
+</script>
+<template>
+    <div 
+        :class="styles.buttonColor"
+        class="rotate-45 p-3">
+        <v-icon class="-rotate-45" :color="styles.buttonTextColor">{{ social.icon }}</v-icon>
+    </div>
+</template>
