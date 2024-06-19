b8527a34f8d89b4274b835dede5250f8d6909c80
filipkotlowski <cotlowzky@gmail.com>
2024-06-19 18:27:52 +0200

    Feature: responsive hero section

--- frontend/src/templates/bari/components/hero-section/HeroSection.vue
+++ frontend/src/templates/bari/components/hero-section/HeroSection.vue
@@ -6,7 +6,7 @@ const data = {
     title: 'Section title is simply dummy text',
     subtitle: 'Section subtitle Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     button: 'lorem ipsum',
-    image: 'https://placehold.co/378x270',
+    image: 'https://placehold.co/500x700',
     styles: {
         sectionBackground: 'bg-[#7AC5FF]',
         headerColor: 'text-[#EEF8FF]',
@@ -19,21 +19,21 @@ const data = {
 </script>
 
 <template>
-    <section :class="data.styles.sectionBackground" class="h-screen px-7 py-10">
-        <div class="h-screen flex justify-between items-center">
+    <section :class="data.styles.sectionBackground" class="h-screen px-7 py-10 md:flex md:justify-center">
+        <div class="h-screen flex justify-between items-center md:w-4/5">
             <div class="w-full flex align-center gap-[20px]">
                 <div class="w-2/3 flex flex-col gap-[52px]">
                     <header-text 
                         :class="data.styles.headerColor" 
-                        class="drop-shadow-primary">{{ data.title}}</header-text>
-                    <span :class="data.styles.subHeaderColor" class="font-GelasioItalic">{{ data.subtitle }}</span>
+                        class="drop-shadow-primary md:!text-[50px]">{{ data.title}}</header-text>
+                    <span :class="data.styles.subHeaderColor" class="font-GelasioItalic md:!text-[20px]">{{ data.subtitle }}</span>
                     <basic-button
                         :class="[data.styles.buttonColor, data.styles.buttonTextColor]">{{ data.button }}</basic-button>
                 </div>
                 <div class="w-1/2 flex ">
                     <img 
                         :class="data.styles.imageBorderColor"
-                        class="ml-7 object-cover object-left h-[428px] w-[207px] border-[9px] border-r-0 border-solid drop-shadow-primary"
+                        class="object-cover object-left h-[428px] w-[207px] border-[9px] border-r-0 border-solid drop-shadow-primary max-[580px]:ml-7 sm:w-[400px] sm:h-[700px] sm:border-r-[9px] sm:object-center min-[580px]:!ml-11"
                         :src="data.image" />
                 </div>
             </div>
