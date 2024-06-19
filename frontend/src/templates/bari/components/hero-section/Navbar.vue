b3b74fcc7343e4dc9669961f337d0e03f49bc426
filipkotlowski <cotlowzky@gmail.com>
2024-06-19 17:54:11 +0200

    Feature: display navbar only on mobile

--- frontend/src/templates/bari/components/hero-section/Navbar.vue
+++ frontend/src/templates/bari/components/hero-section/Navbar.vue
@@ -1,15 +1,8 @@
-<template>
-  <v-layout>
-    <v-bottom-navigation :elevation="0" grow :bg-color="data.styles.navigationBackground" :class="data.styles.navigationTextColor" :exact="true">
-      <v-btn :value="item.name" v-for="item in data.items">      
-        <v-icon>mdi-history</v-icon>
 
-        <span>{{ item.name }}</span>
-      </v-btn>
-    </v-bottom-navigation>
-  </v-layout>
-</template>
 <script setup>
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
 const data = {
   styles: {
     navigationBackground: '#7AC5FF',
@@ -36,3 +29,15 @@ const data = {
 }
 
 </script>
+
+<template>
+  <v-layout v-if="mobile">
+    <v-bottom-navigation :elevation="0" grow :bg-color="data.styles.navigationBackground" :class="data.styles.navigationTextColor" :exact="true">
+      <v-btn :value="item.name" v-for="item in data.items">      
+        <v-icon>mdi-history</v-icon>
+
+        <span>{{ item.name }}</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
