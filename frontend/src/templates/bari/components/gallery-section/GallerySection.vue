3368274e57fa51a6e8dafba25dceb08d06f1737a
filipkotlowski <cotlowzky@gmail.com>
2024-07-06 15:42:59 +0200

    Feature resposnvie gallery section

--- frontend/src/templates/bari/components/gallery-section/GallerySection.vue
+++ frontend/src/templates/bari/components/gallery-section/GallerySection.vue
@@ -2,6 +2,9 @@
 import HeaderText from '@/templates/bari/components/HeaderText.vue';
 import SubHeaderText from '@/templates/bari/components/SubHeaderText.vue';
 import Carousel from '@/templates/bari/components/Carousel.vue';
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
 const data = {
     title: 'Section title',
     subtitle: 'Section subtitle Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
@@ -24,8 +27,8 @@ const data = {
     <section :class="data.styles.sectionBackground" class="h-[50vh] px-7 py-10 flex flex-col items-center ">
         <header-text :class="data.styles.headerColor" class="text-center">{{ data.title }}</header-text>
         <sub-header-text :class="data.styles.subHeaderColor" class="text-center">{{ data.subtitle }}</sub-header-text>
-        <Flicking :options="{ circular: true }">
-            <img v-for="(flicker, index) in data.flickers" :key="index" :src="flicker" width="93%" class="pr-[4%]" />
+        <Flicking :options="{ circular: true, renderOnlyVisible: true }">
+            <img v-for="(flicker, index) in data.flickers" :key="index" :src="flicker" :width="mobile ? '93%' : '33%'" :draggable="mobile ? true : false" class="pr-[4%]" />
         </Flicking>
     </section>
 </template>
